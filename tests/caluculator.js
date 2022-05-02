6f6b211381a593f277283025141d994ccf855860
MadhuSudhan karetigit <madhusudhankareti@gmail.com>
2022-05-02 21:46:25 +0530

    add caluculator

---
+++ tests/caluculator.js
@@ -0,0 +1,33 @@
+describe('demo caluculator tests',function(){
+    it('addition test',function() {
+        browser.get('https://juliemr.github.io/protractor-demo/');
+
+        element(by.model('first')).sendKeys('2');
+
+        element(by.model('second')).sendKeys('6');
+
+        element(by.css('[ng-click="doAddition()"]')).click();
+
+        let result = element(by.cssContainingText('.ng-binding','8'));
+        expect(result.getText()).toEqual('8');
+
+        browser.sleep(3000)
+
+    });
+    it('subtraction test',function() {
+        browser.get('https://juliemr.github.io/protractor-demo/');
+
+        element(by.model('first')).sendKeys('2');
+
+        element(by.model('second')).sendKeys('6');
+
+        element(by.css('[ng-click="doSubtraction()"]')).click();
+
+        let result = element(by.cssContainingText('.ng-binding','8'));
+        expect(result.getText()).toEqual('8');
+
+        browser.sleep(3000)
+
+    });
+    
+});
