1b2fc5a4affa80794404d28b37dae1e9811d49f4
MadhuSudhan karetigit <madhusudhankareti@gmail.com>
2022-05-02 23:56:19 +0530

    subtraction

--- tests/caluculator.js
+++ tests/caluculator.js
@@ -17,14 +17,14 @@ describe('demo caluculator tests',function(){
     it('subtraction test',function() {
         browser.get('https://juliemr.github.io/protractor-demo/');
 
-        element(by.model('first')).sendKeys('2');
+        element(by.model('first')).sendKeys('6');
 
-        element(by.model('second')).sendKeys('6');
+        element(by.model('second')).sendKeys('2');
 
         element(by.css('[ng-click="doSubtraction()"]')).click();
 
-        let result = element(by.cssContainingText('.ng-binding','8'));
-        expect(result.getText()).toEqual('8');
+        let result = element(by.cssContainingText('.ng-binding','4'));
+        expect(result.getText()).toEqual('4');
 
         browser.sleep(3000)
 
